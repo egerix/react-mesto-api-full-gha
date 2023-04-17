1e78cf6e2403fac68a524aaac973d69cedaf523c
Egor Ivanov <egerix@gmail.com>
2023-04-18 01:38:33 +0200

    fix port

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { handleError } = require('./middlewares/handleErrors');
 const config = require('./utils/config');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const limiter = rateLimit(config.limiter);
 
